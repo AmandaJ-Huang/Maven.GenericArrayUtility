ae6f7245ffb30d86254521f8a37b94ebc7bce89f
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-27 12:04:00 -0500

    lab completed

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 /**
  * Created by leon on 3/6/18.
  */
@@ -11,18 +14,59 @@ public class ArrayUtility<T> {
     }
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
-        return 0;
+        Integer counter = 0;
+
+        for (int i = 0; i < arrayToMerge.length; i++) {
+            if(arrayToMerge[i].equals(valueToEvaluate)) {
+                counter++;
+            }
+        }
+
+        for (int i = 0; i < this.array.length; i++) {
+            if(this.array[i].equals(valueToEvaluate)) {
+                counter++;
+            }
+        }
+
+        return counter;
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        return null;
+        Integer counter = 0;
+        T mostCommon = null;
+        List<T> newArray = new ArrayList<>(Arrays.asList(array));
+        newArray.addAll(Arrays.asList(arrayToMerge));
+
+        for (T element : newArray) {
+            if (getNumberOfOccurrences(element) > counter) {
+                counter += getNumberOfOccurrences(element);
+                mostCommon = element;
+            }
+        }
+
+        return mostCommon;
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
-        return 0;
+        Integer counter = 0;
+
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i].equals(valueToEvaluate)) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public T[] removeValue(T valueToRemove) {
-        return null;
+        List<T> newArray = new ArrayList<>();
+
+        for (int i = 0; i < this.array.length; i++) {
+            if (!this.array[i].equals(valueToRemove)) {
+                newArray.add(this.array[i]);
+            }
+        }
+
+        return newArray.toArray(Arrays.copyOf(this.array, newArray.size()));
     }
 }
