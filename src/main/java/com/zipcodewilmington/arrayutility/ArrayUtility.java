17d1f28849f649e138d10f28d5d1e542e39a3ae4
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-26 23:41:38 -0500

    set up methods in ArrayUtility

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,26 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    private T[] array;
+
+    public ArrayUtility(T[] array) {
+        this.array = array;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return 0;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return 0;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
 }
